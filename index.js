4ba58d339b375a76a2eec6b9b8da384c300473ee
zacknjenga <izackkiarie@gmail.com>
2024-04-04 14:58:51 +0300

    adding intro to funstions

---
+++ index.js
@@ -0,0 +1,47 @@
+function shout(string) {
+    return string.toUpperCase(); 
+}
+
+function whisper(string) {
+    return string.toLowerCase("hello"); 
+}
+
+function logWhisper(string){
+    console.log('hello')
+}
+
+function logShout(string){
+    console.log('HELLO')
+}
+
+function sayHiToHeadphonedRoommate(string){
+    return string.toLowerCase("I can't hear you!")
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toUpperCase()) {
+      return "YES INDEED!";
+    } else {
+      // Handle other cases if needed
+      return "Hello!";
+    }
+  
+}
+  function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toLowerCase()) {
+      return "I can't hear you!";
+    } else {
+      // Handle other cases if needed
+      return "YES INDEED!";
+    }
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toUpperCase()) {
+      return "YES INDEED!";
+    } else if (string === "Let's have dinner together!") {
+      return "I would love to!";
+    } else {
+      return "I can't hear you!";
+    }
+  }
